5e7ac878f45396af007d62fb9a55e5961bc165b5
Lorenço <lorenco.alvareznavarrete@gmail.com>
2021-12-04 19:03:05 +0000

    cleanup by removing commented out code in index.jss and lint all files

--- index.js
+++ index.js
@@ -14,60 +14,8 @@ nextISSTimesForMyLocation((error, passTimes) => {
     const duration = passTimeObject.duration;
     return `Next pass at ${date} for ${duration} seconds!`;
   };
-  passTimesArray = passTimes.map(obj => makeReport(obj));
+
+  const passTimesArray = passTimes.map(obj => makeReport(obj));
   console.log(passTimesArray.join('\n'));
-})
 
-
-// fetchMyIP((error, ip) => {
-
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:', ip);
-//   fetchcoordsByIP(ip, (error, coords) => {
-//     if (error) {
-//       console.log(`There was an error!: `, error);
-//       return;
-//     }
-
-//     console.log('Got some coordinates: ', coords);
-//     fetchISSFlyOverTimes(coords, (error, flyovers) => {
-//       if (error) {
-//         console.log(`There was an error!:`, error);
-//         return;
-//       }
-
-//       console.log('These are the flyovertimes:', flyovers);
-
-//     });
-
-//   });
-
-// });
-
-// fetchcoordsByIP('IP goes here', (error, data) => {
-//   if (error) {
-//     console.log(`There was an error!: `, error);
-//     return;
-//   }
-
-//   console.log('Got some coordinates: ', data);
-
-
-// });
-
-
-
-
-// fetchISSFlyOverTimes({ latitude: '55.1834',  longitude: '-118.7457' }, (error, flyovers) => {
-//   if (error) {
-//     console.log(`There was an error!:`, error);
-//     return;
-//   }
-
-//   console.log('These are the flyovertimes:', flyovers);
-
-// });
+});
